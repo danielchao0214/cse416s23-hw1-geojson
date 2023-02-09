7c0f7fef8025ba1fcbda0ed8485d2f6b858cb945
Scott Imandt <scottimandt@gmail.com>
2023-02-09 16:30:28 -0500

    Finished GeoJson map view and continent name change

---
+++ app/src/components/MainMap.js
@@ -0,0 +1,122 @@
+import React, {Component} from "react";
+ 
+import { MapContainer, GeoJSON, TileLayer} from "react-leaflet";
+import  northAmerica  from "./../geoJson Files/data.json";
+import "leaflet/dist/leaflet.css";
+import './MainMap.css'
+
+class MainMap extends Component {
+    state = {};
+   
+    // default style fot the GeoJson file on Load
+    countryStyle = {
+        fillColor: "red",
+        fillOpacity: 0.15,
+        color: "blue",
+        weight: 1
+    }
+
+    getCountryName(country){
+        return country.properties.name
+    }
+
+    setCountryName(country, name){
+        return country.properties.name = name;
+    }
+
+    // this handle the submit for the form in "onEachCountry" function
+    handleForm(event, country, layer){
+        console.log(event);
+        event.preventDefault();
+        let newName = document.getElementById("textfield").value;
+        this.setCountryName(country, newName);
+
+        layer.closePopup();
+        
+    }
+
+    // This function is loaded for every feature in the GeoJSON Object
+
+    onEachCountry = (country, layer) =>{
+        layer.bindPopup(this.getCountryName(country));
+
+        layer.on({
+            mouseover:(event)=>{
+                event.target.setStyle({
+                    fillColor:"yellow",
+                    fillOpacity: 0.2,
+                })
+            },
+
+            mouseout:(event) => {              
+                event.target.setStyle({
+                    fillColor:"red",
+                    fillOpacity: 0.15,
+                })
+            },
+
+
+            click:(event) => {
+                layer.bindPopup(this.getCountryName(country));  
+                event.target.setStyle({
+                    fillOpacity: .5,
+                    fillColor:"green"
+                })
+            },
+
+            dblclick:(event) => {      
+                
+                // form for inside the popup window
+                var x = document.createElement("FORM");
+                x.setAttribute("id", "myForm");
+                x.addEventListener('submit', (event) => {this.handleForm(event, country, layer)});
+                document.body.appendChild(x);
+
+                //textfield to add to the form
+                var y = document.createElement("INPUT");
+                y.setAttribute("id","textfield");
+                y.setAttribute("type", "text");
+                y.setAttribute("value", this.getCountryName(country));
+                document.getElementById("myForm").appendChild(y);           
+                
+                //set the popup to the form
+                layer.bindPopup(x);
+
+                //change color to reflect double click
+                event.target.setStyle({
+                    fillColor:"red",
+                    fillOpacity: 0.5,
+                })
+
+            }
+
+        })
+    }
+
+    render() {
+
+        return (  
+
+        <div className ="Map">
+           <MapContainer
+                style={{height:"80vh", width:"80vw"}}
+                zoom={4}
+                center= {[39.63568661947377, -102.07068046738928]}
+                doubleClickZoom = {false}
+            >
+                {/* This is where the GeoJSON file is Loaded inthe Data section*/}
+                <GeoJSON style= {this.countryStyle} data={northAmerica.features} onEachFeature={this.onEachCountry}/>
+
+               <TileLayer
+                attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+                url = "https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=Sp0j87JpClPB8cs9tlUR"
+               >
+                
+                </TileLayer>
+            </MapContainer>
+        </div>  
+        )
+    }
+}
+
+export default MainMap;
