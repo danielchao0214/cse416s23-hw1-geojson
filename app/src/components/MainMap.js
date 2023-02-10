8a2e54086c687140d26cc7cc41ee809e2df324f3
Daniel Chao <danielchao0214@gmail.com>
2023-02-09 20:54:24 -0500

    works with refresh

--- app/src/components/MainMap.js
+++ app/src/components/MainMap.js
@@ -1,44 +1,50 @@
-import React, { Component } from "react";
-
-import { MapContainer, GeoJSON, TileLayer } from "react-leaflet";
-import northAmerica from "./../geoJson Files/data.json";
+import React, { useState } from "react";
+import { MapContainer, GeoJSON, TileLayer, useMap } from "react-leaflet";
+import L from 'leaflet';
+// import northAmerica from "./../geoJson Files/data.json";\
 import "leaflet/dist/leaflet.css";
 import './MainMap.css'
 
-class MainMap extends Component {
-    state = { map: {} };
+const MainMap = (props) => {
+    const [currentGeoJSON, setCurrentGeoJSON] = useState({});
+    function MyComponent() {
+        const map = useMap();
+        console.log(map.getCenter());
+        return null;
+    }
 
     // default style fot the GeoJson file on Load
-    countryStyle = {
+    let countryStyle = {
         fillColor: "red",
         fillOpacity: 0.15,
         color: "blue",
         weight: 1
     }
 
-    getCountryName(country) {
+    function getCountryName(country) {
         return country.properties.name
     }
 
-    setCountryName(country, name) {
+    function setCountryName(country, name) {
         return country.properties.name = name;
     }
-    showFile = (e) => {
+
+    function showFile(e) {
         e.preventDefault();
         const reader = new FileReader();
         reader.onload = (e) => {
             const text = e.target.result;
-            this.setState(({ map }) => ({ map: JSON.parse(text) }));
+            setCurrentGeoJSON(JSON.parse(text));
         };
         reader.readAsText(e.target.files[0]);
-    };
+    }
 
     // this handle the submit for the form in "onEachCountry" function
-    handleForm(event, country, layer) {
+    function handleForm(event, country, layer) {
         console.log(event);
         event.preventDefault();
         let newName = document.getElementById("textfield").value;
-        this.setCountryName(country, newName);
+        setCountryName(country, newName);
 
         layer.closePopup();
 
@@ -46,8 +52,8 @@ class MainMap extends Component {
 
     // This function is loaded for every feature in the GeoJSON Object
 
-    onEachCountry = (country, layer) => {
-        layer.bindPopup(this.getCountryName(country));
+    function onEachCountry(country, layer) {
+        layer.bindPopup(getCountryName(country));
 
         layer.on({
             mouseover: (event) => {
@@ -66,7 +72,7 @@ class MainMap extends Component {
 
 
             click: (event) => {
-                layer.bindPopup(this.getCountryName(country));
+                layer.bindPopup(getCountryName(country));
                 event.target.setStyle({
                     fillOpacity: .5,
                     fillColor: "green"
@@ -78,14 +84,14 @@ class MainMap extends Component {
                 // form for inside the popup window
                 var x = document.createElement("FORM");
                 x.setAttribute("id", "myForm");
-                x.addEventListener('submit', (event) => { this.handleForm(event, country, layer) });
+                x.addEventListener('submit', (event) => { handleForm(event, country, layer) });
                 document.body.appendChild(x);
 
                 //textfield to add to the form
                 var y = document.createElement("INPUT");
                 y.setAttribute("id", "textfield");
                 y.setAttribute("type", "text");
-                y.setAttribute("value", this.getCountryName(country));
+                y.setAttribute("value", getCountryName(country));
                 document.getElementById("myForm").appendChild(y);
 
                 //set the popup to the form
@@ -102,31 +108,40 @@ class MainMap extends Component {
         })
     }
 
-    render() {
-        return (
-            <div className="Map">
-                <MapContainer
-                    style={{ height: "80vh", width: "80vw" }}
-                    zoom={4}
-                    center={[39.63568661947377, -102.07068046738928]}
-                    doubleClickZoom={false}
+
+
+    return (
+        <div className="Map">
+            <div id="leafletmap"></div>
+            <MapContainer
+                style={{ height: "80vh", width: "80vw" }}
+                zoom={4}
+                center={[39.63568661947377, -102.07068046738928]}
+                doubleClickZoom={false}
+                id="leafletmap"
+            >
+                <MyComponent />
+                {/* This is where the GeoJSON file is Loaded in the Data section*/}
+                {currentGeoJSON.features && (
+                    <GeoJSON
+                        style={countryStyle}
+                        data={currentGeoJSON.features}
+                        onEachFeature={onEachCountry}
+                    />
+                )}
+
+                <TileLayer
+                    attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+                    url="https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=Sp0j87JpClPB8cs9tlUR"
                 >
-                    {/* This is where the GeoJSON file is Loaded inthe Data section*/}
-                    <GeoJSON style={this.countryStyle} data={this.state.map.features} onEachFeature={this.onEachCountry} />
 
-                    <TileLayer
-                        attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-                        url="https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=Sp0j87JpClPB8cs9tlUR"
-                    >
-
-                    </TileLayer>
-                </MapContainer>
-                <div>
-                    <input type="file" onChange={this.showFile} />
-                </div >
+                </TileLayer>
+            </MapContainer>
+            <div>
+                <input type="file" onChange={showFile} />
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default MainMap;
